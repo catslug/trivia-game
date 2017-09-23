27ab8b60e0fa7851422a12f7e9e25556594661c5
Lindsay Wall <lindsay.is@gmail.com>
2017-09-23 18:18:24 -0400

    questions now nested in an object, added question 3 and 4

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,22 +1,88 @@
- var qs = {
- 	q1: ["This gentle alien race looks a little like cthulu but carries a glowing sphere which functions as a speaking device:"],
- 	q1a: "The Ood",
- 	q1no: ["Weeping Angels", "Adipose", "Cybermen"],
- 	q1img: "https://media.giphy.com/media/WE5hRLWRWalwY/giphy.gif",
- 	q2: "He just wants to go home. Can’t he just borrow your phone?",
- 	q2a: "E.T.",
- 	q2no: ["Alf", "Answer No", "Answer No Too"],
- 	q2img: "https://media.giphy.com/media/gHcPh3ehbRGik/giphy.gif",
+// NEED TO TELL IT WHAT TO DO WHEN YOU GET TO THE LAST QUESTION.
+var qs = {
+  hereMyQuestions: [{
+    question: "This gentle alien race looks a little like cthulu but carries a glowing sphere which functions as a speaking device:",
+    yesAnswer: "The Ood",
+    noAnswer: ["Weeping Angels", "Adipose", "Cybermen"],
+    ansImg: "https://media.giphy.com/media/WE5hRLWRWalwY/giphy.gif",
+  }, { 
+    question: "He just wants to go home. Can’t he just borrow your phone?",
+    yesAnswer: "E.T.",
+    noAnswer: ["Alf", "Answer No", "Answer No Too"],
+    ansImg: "https://media.giphy.com/media/gHcPh3ehbRGik/giphy.gif",
+  }, {
+    question: "She’s no longer the heir to the throne. In search of herself, she runs off to live among wolves in the woods of Ooo, was subsequently banished, and proceeded to terrorize a local village in order to eat their crops. This princess is:",
+    yesAnswer: "Lumpy Space Princess",
+    noAnswer: ["Flame Princess", "Princess Bubblegum", "Princess Leia"],
+    ansImg: "https://media.giphy.com/media/3eT5kJmvLl8TC/giphy.gif",
+  }, {
+  	question: "A parody mirror character to Star Wars’ giant-eyed, worm-like villain, this character is known as:",
+  	yesAnser: "Pizza the Hut",
+  	noAnswer: ["Jabberwocky", "Bubble Tea Fet", "blah"]
+  	ansImg: "https://media.giphy.com/media/e9naBh3ziasN2/giphy.gif"
+  }, {
+  	question: "The universe’s twisted pest control, this vicious species wears a tough mechanical exterior over their delicate inner bodies and approach all enemies while chanting “exterminate”.",
+  	yesAnser: "Daleks",
+  	noAnswer: ["Sontaran", "Slitheen", "Time Lords"]
+  	ansImg: 
+  }, {
+  	question: "Blah Blah",
+  	yesAnser: "Blah",
+  	noAnswer: []
+  	ansImg: 
+  }, {
+  	question: "Blah Blah",
+  	yesAnser: "Blah",
+  	noAnswer: []
+  	ansImg: 
+  }, {
+  	question: "Blah Blah",
+  	yesAnser: "Blah",
+  	noAnswer: []
+  	ansImg: 
+  }, {
+  	question: "Blah Blah",
+  	yesAnser: "Blah",
+  	noAnswer: []
+  	ansImg: 
+  }, {
+  	question: "Blah Blah",
+  	yesAnser: "Blah",
+  	noAnswer: []
+  	ansImg: 
+  }, {
+  	question: "Blah Blah",
+  	yesAnser: "Blah",
+  	noAnswer: []
+  	ansImg: 
+  }, {
+  	question: "Blah Blah",
+  	yesAnser: "Blah",
+  	noAnswer: []
+  	ansImg: 
+  }, {
+  	question: "Blah Blah",
+  	yesAnser: "Blah",
+  	noAnswer: []
+  	ansImg: 
+  }, {
+  	question: "Blah Blah",
+  	yesAnser: "Blah",
+  	noAnswer: []
+  	ansImg: 
+  }, {
+  	question: "Blah Blah",
+  	yesAnser: "Blah",
+  	noAnswer: []
+  	ansImg: 
+  }]
 }
 var timeClock = 30
 var answers = []
 var correctAnswers = 0
 var wrongAnswers = 0
 var myTimer
-var currentQ = qs.q1;
-var currentAns = qs.q1a;
-var currentDecoy = qs.q1no;
-var currentImg = qs.q1img;
+var currentQ = 0
 
 // start button activation
 $(document).ready(function startButton() {
@@ -29,7 +95,7 @@ $(document).ready(function startButton() {
 	 	$("#ongo").addClass("hidden");
 
 	 	// writes the question to the page 
-	 	$("#question").text(currentQ);
+	 	$("#question").text(qs.hereMyQuestions[currentQ].question);
 
 		answerArray();
 		renderAnswers();
@@ -40,7 +106,7 @@ function start() {
 		// writes the question to the page 
 		$("#timer").text("Seconds left: " + timeClock);
 	 	qClock();
-	 	$("#question").text(currentQ);
+	 	$("#question").text(qs.hereMyQuestions[currentQ].question);
 		answerArray();
 		renderAnswers();
 }
@@ -48,9 +114,9 @@ function start() {
 // creates array of answers
 function answerArray() {
 	// pushes all of the possible answers to the answers array
-	answers.push(currentAns);
+	answers.push(qs.hereMyQuestions[currentQ].yesAnswer);
  	for (var i = 0; i < 3; i++) {
- 		answers.push(currentDecoy[i]);
+ 		answers.push(qs.hereMyQuestions[currentQ].noAnswer[i]);
  	}
 
  	// sorts the new array of answers
@@ -59,7 +125,7 @@ function answerArray() {
 	});
 }
 
-// renders ansers to the DOM
+// renders answers to the DOM
 function renderAnswers() {
 	// creates a p tag to display the answers on the page vertically and assigns hover class 
 	for (var i = 0; i < answers.length; i++) {
@@ -85,16 +151,13 @@ function qClock() {
 			removeQ();
 			timesUp();
 			// I think I'm going to need nested objects.
-			currentQ = qs.q2;
-			currentAns = qs.q2a;
-			currentDecoy = qs.q2no;
-			currentImg = qs.q2img;
+			currentQ++;
 			start();
       	}
     }
 }
 
-// stops and clears the timer
+// stops and clears the timer, resets value
 function stop() {
   clearInterval(myTimer);
   timeClock = 30;
@@ -106,17 +169,14 @@ $(document).on("click", ".hoverThing", function() {
 	var userAnswer = this
 	console.log(this);
 
-	if (($(this).attr("data-value")) === currentAns) {
+	if (($(this).attr("data-value")) === qs.hereMyQuestions[currentQ].yesAnswer) {
 		console.log("winner!");
 		correctAnswers++;
 		answers = [];
 		stop();
 		removeQ();
 		resultYes();
-		currentQ = qs.q2;
-		currentAns = qs.q2a;
-		currentDecoy = qs.q2no;
-		currentImg = qs.q2img;	
+		currentQ++;	
 	}
 
 	else {
@@ -126,10 +186,7 @@ $(document).on("click", ".hoverThing", function() {
 		stop();
 		removeQ();
 		resultNo();
-		currentQ = qs.q2;
-		currentAns = qs.q2a;
-		currentDecoy = qs.q2no;
-		currentImg = qs.q2img;
+		currentQ++; 
 	}
 });
 
@@ -144,24 +201,24 @@ function removeQ() {
 function resultYes() {
 	myTimer = setTimeout(yesDelete, 3000);
 
-	$("#question").text("That's right!  It's " + currentAns + ".");
-	$("<img src='" + currentImg + "'' />").appendTo("#img-result");
+	$("#question").text("That's right!  It's " + qs.hereMyQuestions[currentQ].yesAnswer + ".");
+	$("<img src='" + qs.hereMyQuestions[currentQ].ansImg + "'' />").appendTo("#img-result");
 }
 
 // displays this text for 3 seconds if wrong answer
 function resultNo() {
 	myTimer = setTimeout(yesDelete, 3000);
 
-	$("#question").text("Nope!  It's " + currentAns + ".");
-	$("<img src='" + currentImg + "'' />").appendTo("#img-result");
+	$("#question").text("Nope!  It's " + qs.hereMyQuestions[currentQ].yesAnswer + ".");
+	$("<img src='" + qs.hereMyQuestions[currentQ].ansImg + "'' />").appendTo("#img-result");
 }
 
 // displays this text for 3 seconds if time runs out
 function timesUp() {
 	myTimer = setTimeout(yesDelete, 3000);
 
-	$("#question").text("Time's Up!  The correct answer is: " + currentAns + ".");
-	$("<img src='" + currentImg + "'' />").appendTo("#img-result");
+	$("#question").text("Time's Up!  The correct answer is: " + qs.hereMyQuestions[currentQ].yesAnswer + ".");
+	$("<img src='" + qs.hereMyQuestions[currentQ].ansImg + "'' />").appendTo("#img-result");
 }
 // deletes result information from the DOM
 function yesDelete() {
